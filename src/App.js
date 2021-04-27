e02eb109c3914d6b4bb3a4f2191846f5fe39f423
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-04-27 13:45:04 -0500

    setting up about component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+
 
 function App() {
   return (
---
+++ src/components/About/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section>
+            <div className="center" id="about">
+            <h1 className="page-header">About Me</h1>
+                </div>
+                <div className="center">
+                    <img src={require()} />
+                </div>
+            
+
+            
+        </section>
+        
+    );
+}
+
+export default About;
